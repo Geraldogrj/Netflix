471959ac331ec7238c2b68284a41640b06d7d8aa
Roberto Dantas <7robertodantas@gmail.com>
2016-10-05 20:24:39 -0300

    Ajustes hibernate
    Signed-off-by: Roberto Dantas <7robertodantas@gmail.com>

---
+++ src/main/java/dao/HibernateUtil.java
@@ -0,0 +1,33 @@
+package dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// load from different directory
+			SessionFactory sessionFactory = new Configuration().configure(
+					"/netflix/src/main/resources/hibernate.cfg.xml")
+					.buildSessionFactory();
+			return sessionFactory;
+
+		} 
+		catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+
+}
---
+++ src/main/java/model/Media.java
@@ -0,0 +1,151 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Geraldo
+ */
+@Entity
+@Table(name = "Media")
+public class Media implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+    private int id;
+    private String nome;
+    private String descricao;
+    private int ano;
+    private int temporada;
+    private int episodio;
+    private Calendar duracao;
+    private String categoria;
+    private String diretor;
+    private String protagonista;
+    private int idade;
+    private ImageView imagem;
+    
+    public Media(){
+        
+    }
+
+    public Media(int id, String nome, String descricao, int ano, int temporada, int episodio, Calendar duracao, String categoria, String diretor, String protagonista, int idade, ImageView imagem) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.ano = ano;
+        this.temporada = temporada;
+        this.episodio = episodio;
+        this.duracao = duracao;
+        this.categoria = categoria;
+        this.diretor = diretor;
+        this.protagonista = protagonista;
+        this.idade = idade;
+        this.imagem = imagem;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(int temporada) {
+        this.temporada = temporada;
+    }
+
+    public int getEpisodio() {
+        return episodio;
+    }
+
+    public void setEpisodio(int episodio) {
+        this.episodio = episodio;
+    }
+
+    public Calendar getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(Calendar duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public String getProtagonista() {
+        return protagonista;
+    }
+
+    public void setProtagonista(String protagonista) {
+        this.protagonista = protagonista;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public ImageView getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(ImageView imagem) {
+        this.imagem = imagem;
+    }
+    
+    
+    
+}

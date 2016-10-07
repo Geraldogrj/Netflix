1264174240d1d2c723d6b20357fd1cb3b80ab57d
Geraldogrj <geraldogrj@gmail.com>
2016-10-07 17:22:33 -0300

    Alterações no LoginController

--- src/main/java/controller/LoginAppController.java
+++ src/main/java/controller/LoginAppController.java
@@ -1,12 +1,19 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -25,8 +32,13 @@ public class LoginAppController implements Initializable {
     private ImageView imgNetflix;
    
     @FXML
-    public void logar(){
-        System.out.println("Clicou!");
+    public void logar(ActionEvent event){
+        try {
+            abrirTela(event);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 
     @Override
@@ -34,4 +46,13 @@ public class LoginAppController implements Initializable {
 
     }
     
+    public void abrirTela(ActionEvent event) throws IOException{
+        Parent media = FXMLLoader.load(getClass().getResource("/fxml/MediaOverview.fxml"));
+        Scene mediaScene = new Scene(media);
+        Stage mediaTela = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //TODO: Não estou conseguindo gerar a tela centralizada!!!
+        mediaTela.setTitle("Media Overview");
+        mediaTela.setScene(mediaScene);
+        mediaTela.show();
+    }
 }

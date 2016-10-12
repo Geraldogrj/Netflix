38c8d2422ef88d98a03d0c2f0c2fe6c267391479
Geraldogrj <geraldogrj@gmail.com>
2016-10-12 19:14:14 -0300

    remover midia

--- src/main/java/br/ufrn/imd/netflix/application/controller/AdminController.java
+++ src/main/java/br/ufrn/imd/netflix/application/controller/AdminController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import org.hibernate.event.spi.SaveOrUpdateEvent;
 
 /**
@@ -22,48 +23,50 @@ import org.hibernate.event.spi.SaveOrUpdateEvent;
  * @author Geraldo e Roberto
  */
 public class AdminController extends Controller {
-    
-    public static final String FXML_ADMIN = "/fxml/admin_view.fxml";
-    	
-	@FXML 
-	private Label lblUsuarioLogado;
-        @FXML
-        private TextField idUser;
-        @FXML
-        private TextField nomeUser;
-        @FXML
-        private TextField login;
-        @FXML
-        private TextField senha;
-        
-        @FXML
-        private TextField id;
-        @FXML
-        private TextField nome;
-        @FXML
-        private TextField descricao;
-        @FXML
-        private TextField ano;
-        @FXML
-        private TextField temporada;
-        @FXML
-        private TextField episodio;
-        @FXML
-        private TextField duracao;
-        @FXML
-        private TextField categoria;
-        @FXML
-        private TextField diretor;
-        @FXML
-        private TextField protagonista;
-        @FXML
-        private TextField idade;
 
-	@Override
-	public void onCreate(Bundle bundle) {
-		Usuario usuario = (Usuario) bundle.get("usuario");
-		lblUsuarioLogado.setText(usuario.getNome());
-	}
+    public static final String FXML_ADMIN = "/fxml/admin_view.fxml";
+
+    @FXML
+    private Label lblUsuarioLogado;
+    @FXML
+    private TextField idUser;
+    @FXML
+    private TextField nomeUser;
+    @FXML
+    private TextField login;
+    @FXML
+    private TextField senha;
+
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField nome;
+    @FXML
+    private TextField descricao;
+    @FXML
+    private TextField ano;
+    @FXML
+    private TextField temporada;
+    @FXML
+    private TextField episodio;
+    @FXML
+    private TextField duracao;
+    @FXML
+    private TextField categoria;
+    @FXML
+    private TextField diretor;
+    @FXML
+    private TextField protagonista;
+    @FXML
+    private TextField idade;
+    @FXML
+    private ImageView icoSearch;
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        Usuario usuario = (Usuario) bundle.get("usuario");
+        lblUsuarioLogado.setText(usuario.getNome());
+    }
 
     public Label getLblUsuarioLogado() {
         return lblUsuarioLogado;
@@ -192,67 +195,102 @@ public class AdminController extends Controller {
     public void setIdade(TextField idade) {
         this.idade = idade;
     }
-    
-    public void cadastrar(){
-        try{
-        Dao<Usuario> dao = getDAO(Usuario.class);
-        
-        Usuario usuario = new Usuario();
-        usuario.setId(parseInt(idUser.getText()));
-        usuario.setNome(nomeUser.getText());
-        usuario.setLogin(login.getText());
-        usuario.setSenha(senha.getText());
-        
-        dao.saveOrUpdate(usuario);
+
+    public void cadastrar() {
+        try {
+            Dao<Usuario> dao = getDAO(Usuario.class);
+
+            Usuario usuario = new Usuario();
+            usuario.setId(parseInt(idUser.getText()));
+            usuario.setNome(nomeUser.getText());
+            usuario.setLogin(login.getText());
+            usuario.setSenha(senha.getText());
+
+            dao.saveOrUpdate(usuario);
             abrirAlertaInfo("Sucesso", "Usuário cadastrado/atualizado com sucesso");
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             abrirAlertaErro("Erro", "Não foi possível efetuar o cadastro");
         }
     }
-   
-    public void salvarMidia(){
-       try{
-        Dao<Media> dao = getDAO(Media.class);
-        
-        Media media = new Media();
-        media.setId(parseInt(id.getText()));
-        media.setNome(nome.getText());
-        media.setDescricao(descricao.getText());
-        media.setAno(parseInt(ano.getText()));
-        media.setTemporada(parseInt(temporada.getText()));
-        media.setEpisodio(parseInt(episodio.getText()));
-        media.setDuracao(parseInt(duracao.getText()));
-        media.setCategoria(categoria.getText());
-        media.setDiretor(diretor.getText());
-        media.setProtagonista(protagonista.getText());
-        media.setIdade(parseInt(idade.getText()));
-        
-        dao.saveOrUpdate(media);
-        
-        abrirAlertaInfo("Sucesso" , "Mídia cadastrada/atualizada com sucesso");
-        
-       } catch(Exception e) {
-           e.printStackTrace();
-           abrirAlertaErro("Erro", "Não foi possível cadastrar/atualizar");
-       }
+
+    public void salvarMidia() {
+        try {
+            Dao<Media> dao = getDAO(Media.class);
+
+            Media media = new Media();
+            media.setId(parseInt(id.getText()));
+            media.setNome(nome.getText());
+            media.setDescricao(descricao.getText());
+            media.setAno(parseInt(ano.getText()));
+            media.setTemporada(parseInt(temporada.getText()));
+            media.setEpisodio(parseInt(episodio.getText()));
+            media.setDuracao(parseInt(duracao.getText()));
+            media.setCategoria(categoria.getText());
+            media.setDiretor(diretor.getText());
+            media.setProtagonista(protagonista.getText());
+            media.setIdade(parseInt(idade.getText()));
+
+            dao.saveOrUpdate(media);
+
+            abrirAlertaInfo("Sucesso", "Mídia cadastrada/atualizada com sucesso");
+            apagarCampos();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            abrirAlertaErro("Erro", "Não foi possível cadastrar/atualizar");
+        }
+    }
+
+    public void pesquisarPorID() {
+        try {
+            Dao<Media> dao = getDAO(Media.class);
+
+            Media media = dao.findById(parseInt(id.getText()));
+
+            id.setText(media.getId().toString());
+            nome.setText(media.getNome());
+            descricao.setText(media.getDescricao());
+            ano.setText(media.getAno().toString());
+            temporada.setText(media.getTemporada().toString());
+            episodio.setText(media.getEpisodio().toString());
+            duracao.setText(media.getDuracao().toString());
+            categoria.setText(media.getCategoria());
+            diretor.setText(media.getDiretor());
+            protagonista.setText(media.getProtagonista());
+            idade.setText(media.getIdade().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            abrirAlertaErro("Erro", "Não foi possível achar o item pesquisado");
+        }
+    }
+
+    public void removerMidia() {
+        try {
+            Dao<Media> dao = getDAO(Media.class);
+            Media media = dao.findById(parseInt(id.getText()));
+
+            dao.delete(media);
+            
+            abrirAlertaInfo("Sucesso", "Mídia removida com sucesso");
+            apagarCampos();
+        } catch (Exception e) {
+            e.printStackTrace();
+            abrirAlertaErro("Erro", "Não foi possível remover a Mídia");
+        }
     }
     
-    public void pesquisarPorID(){
-        Dao<Media> dao = getDAO(Media.class);
-        
-        Media media = dao.findById(parseInt(id.getText()));
-        
-        id.setText(media.getId().toString());
-        nome.setText(media.getNome());
-        descricao.setText(media.getDescricao());
-        ano.setText(media.getAno().toString());
-        temporada.setText(media.getTemporada().toString());
-        episodio.setText(media.getEpisodio().toString());
-        duracao.setText(media.getDuracao().toString());
-        categoria.setText(media.getCategoria());
-        diretor.setText(media.getDiretor());
-        protagonista.setText(media.getProtagonista());
-        idade.setText(media.getIdade().toString());
+    public void apagarCampos(){
+        id.setText("");
+        nome.setText("");
+        descricao.setText("");
+        ano.setText("");
+        temporada.setText("");
+        episodio.setText("");
+        duracao.setText("");
+        diretor.setText("");
+        protagonista.setText("");
+        idade.setText("");
+        categoria.setText("");
     }
 }
